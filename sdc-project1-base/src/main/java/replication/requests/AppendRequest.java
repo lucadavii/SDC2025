18b5391baabe7f0d99ac5e15abf3c54d5411a8bb
Luca Davì <lucadavi@MacBook-Air-di-Luca.local>
2025-10-31 17:10:59 +0000

    solved merge error in AppendRequest

--- sdc-project1-base/src/main/java/replication/requests/AppendRequest.java
+++ sdc-project1-base/src/main/java/replication/requests/AppendRequest.java
@@ -92,9 +92,6 @@ public class AppendRequest extends ProtoRequest {
 	public byte[] getClientRequest() {
 		return clientRequest;
 	}
-	public Host getSource() {
-		return source;
-	}
 	public byte[] encode() throws IOException {
 		ByteBuf buf = Unpooled.buffer();
 		buf.writeLong(requestID.getMostSignificantBits());
